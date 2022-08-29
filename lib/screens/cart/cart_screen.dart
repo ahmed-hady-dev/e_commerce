58d8b9cbdf5a48684d94702e4140208667d7edce
ahmed <ahmed.hady.dev@gmail.com>
2022-08-29 16:50:33 +0200

    #07 cart screen ui added

--- lib/screens/cart/cart_screen.dart
+++ lib/screens/cart/cart_screen.dart
@@ -1,14 +1,111 @@
+import 'package:e_commerce/model/product_model.dart';
 import 'package:flutter/material.dart';
+
+import '../../widgets/cart_product_card.dart';
+import '../../widgets/custom_appbar.dart';
+import '../../widgets/custom_nav_bar.dart';
 
 class CartScreen extends StatelessWidget {
   const CartScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('CartScreen'),
-      ),
-    );
+    return Scaffold(
+        appBar: const CustomAppbar(text: 'Cart'),
+        bottomNavigationBar: BottomAppBar(
+            color: Colors.black,
+            child: Container(
+                height: 70.0,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    ElevatedButton(
+                        style: ElevatedButton.styleFrom(primary: Colors.white),
+                        onPressed: () {},
+                        child: Text('GO TO CHECKOUT', style: Theme.of(context).textTheme.headline3))
+                  ],
+                ))),
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Column(
+                children: <Widget>[
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Text('Add \$20.0 for FREE Delivery', style: Theme.of(context).textTheme.headline5),
+                      ElevatedButton(
+                        onPressed: () {},
+                        style: ElevatedButton.styleFrom(
+                            primary: Colors.black, shape: const RoundedRectangleBorder(), elevation: 0.0),
+                        child: Text('Add More Items',
+                            style: Theme.of(context).textTheme.headline5!.copyWith(color: Colors.white)),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 10.0),
+                  CartProductCard(product: Product.products[0]),
+                  CartProductCard(product: Product.products[0]),
+                ],
+              ),
+              Column(
+                children: <Widget>[
+                  const Divider(thickness: 2),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 40.0, vertical: 10.0),
+                    child: Column(
+                      children: <Widget>[
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: <Widget>[
+                            Text('SUBTOTAL', style: Theme.of(context).textTheme.headline5),
+                            Text('\$5.98', style: Theme.of(context).textTheme.headline5),
+                          ],
+                        ),
+                        const SizedBox(height: 10),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: <Widget>[
+                            Text('DELIVERY FEE', style: Theme.of(context).textTheme.headline5),
+                            Text('\$2.90', style: Theme.of(context).textTheme.headline5),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                  Stack(
+                    children: [
+                      Container(
+                        width: MediaQuery.of(context).size.width,
+                        height: 60.0,
+                        decoration: BoxDecoration(color: Colors.black.withAlpha(50)),
+                        child: Container(
+                          width: MediaQuery.of(context).size.width,
+                          height: 50.0,
+                          margin: const EdgeInsets.all(5.0),
+                          decoration: BoxDecoration(color: Colors.black),
+                          child: Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 30.0),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: <Widget>[
+                                Text('TOTAL',
+                                    style: Theme.of(context).textTheme.headline5!.copyWith(color: Colors.white)),
+                                Text('\$12.90',
+                                    style: Theme.of(context).textTheme.headline5!.copyWith(color: Colors.white)),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ],
+          ),
+        ));
   }
 }
---
+++ lib/widgets/cart_product_card.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+import '../model/product_model.dart';
+
+class CartProductCard extends StatelessWidget {
+  final Product product;
+  const CartProductCard({Key? key, required this.product}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 8.0),
+      child: Row(
+        children: <Widget>[
+          Image.network(product.imageUrl, width: 100.0, height: 80.0, fit: BoxFit.cover),
+          const SizedBox(width: 10.0),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(product.name, style: Theme.of(context).textTheme.headline5),
+                Text('\$${product.price}', style: Theme.of(context).textTheme.headline6),
+              ],
+            ),
+          ),
+          const SizedBox(width: 10),
+          Row(
+            children: <Widget>[
+              IconButton(onPressed: () {}, icon: const Icon(Icons.remove_circle)),
+              Text('1', style: Theme.of(context).textTheme.headline5),
+              IconButton(onPressed: () {}, icon: const Icon(Icons.add_circle)),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
